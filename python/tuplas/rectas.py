f2ef28c60a3eac21a382e7d40224374087f63b2c
Daniel <dsanmartinreyes@gmail.com>
2020-11-11 20:25:43 -0400

    Deployed 44b029f with MkDocs version: 1.1.2

--- python/tuplas/rectas.py
+++ python/tuplas/rectas.py
@@ -1,20 +1,9 @@
-"""
-	Ejercicio Rectas
-	http://progra.usm.cl/apunte/ejercicios/2/rectas.html
-"""
-
 # Pregunta 1
 def punto_en_recta(p, r):
 	x, y = p # Desempaquetar punto (x, y)
 	m, b = r # Desempaquetar recta (pendiente, intercepto)
 	# Si un punto pertenece a la recta se cumple que x * m + b = y
 	return y == x * m + b
-
-# Pruebas
-recta = (2, -1) # esta es la recta y = 2x - 1
-print(punto_en_recta((2, 3), recta))
-print(punto_en_recta((0, -1), recta))
-print(punto_en_recta((1, 2), recta))
 
 # Pregunta 2
 def son_paralelas(r1, r2):
@@ -36,14 +25,6 @@ def recta_que_pasa_por(p1, p2):
 	recta = (m, b) # Nueva recta
 	return recta
 
-# Prueba pregunta 3
-print(recta_que_pasa_por((-2, 4), (4, 1)))
-# Verificacion sugerida
-p1 = (-2, 4)
-p2 = (4, 1)
-r = recta_que_pasa_por(p1, p2)
-print(punto_en_recta(p1, r) and punto_en_recta(p2, r))
-
 # Pregunta 4
 def punto_de_interseccion(r1, r2):
 	if son_paralelas(r1, r2): # Rectas paralelas
@@ -60,7 +41,22 @@ def punto_de_interseccion(r1, r2):
 		y = m1 * x + b1
 		return (x, y)
 
-# Pruebas
+# Pruebas #
+# Pregunta 1
+recta = (2, -1) # esta es la recta y = 2x - 1
+print(punto_en_recta((2, 3), recta))
+print(punto_en_recta((0, -1), recta))
+print(punto_en_recta((1, 2), recta))
+
+# pregunta 3
+print(recta_que_pasa_por((-2, 4), (4, 1)))
+# Verificacion sugerida
+p1 = (-2, 4)
+p2 = (4, 1)
+r = recta_que_pasa_por(p1, p2)
+print(punto_en_recta(p1, r) and punto_en_recta(p2, r))
+
+# Pregunta 4
 r1 = (2, 1)
 r2 = (-1, 4)
 print(punto_de_interseccion(r1, r2))
