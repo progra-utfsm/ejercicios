d8872d6ed85099ea0affb78c8d29b1214b09a3cd
Daniel <dsanmartinreyes@gmail.com>
2020-11-16 18:54:18 -0400

    Deployed 44b029f with MkDocs version: 1.1.2

--- python/diccionarios/planetas.py
+++ python/diccionarios/planetas.py
@@ -1,3 +1,4 @@
+# Datos #
 planetas = {
    'Mercurio': (57910000, 4880, 0.241, 58.6, 0.06),
    'Venus': (108200000,12000, 0.72, 243, 0.82),
@@ -9,12 +10,23 @@ planetas = {
    'Neptuno': (4504300000, 49492, 30.66, 0.6745, 17.2)
 }
 
-
-lista = []
+# Preguntas #
+# 1
+lista = [] # Se crea una lista de tuplas para ordenar utilizando el criterio de tuplas
 for planeta in planetas:
-   diametro, _, _, _ = planetas[planeta]
+   _, diametro, _, _, _ = planetas[planeta]
    lista.append((diametro, planeta))
-lista.sort()
-lista.reverse()
+lista.sort() # Orden ascendente
+lista.reverse() # Orden descendente
+# Mostrar los planetas
 for _, planeta in lista:
     print(planeta)
+
+# 2
+lista2 = [] # Misma idea anterior, pero guardando la informacion de rotacion
+for planeta in planetas:
+   _, _, _, rotacion, _ = planetas[planeta]
+   lista2.append((rotacion, planeta))
+lista2.sort() # Orden ascendente
+# Mostrar el planeta 
+print("Planeta con menor período de rotación:", lista2[0][1])
