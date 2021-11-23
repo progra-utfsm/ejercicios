5291e0fdccb5627f0c6924b7295b1c77e810f457
Daniel <dsanmartinreyes@gmail.com>
2021-11-23 15:45:49 -0400

    Deployed 400b0e8 with MkDocs version: 1.1.2

---
+++ python/diccionarios/autopista.py
@@ -0,0 +1,46 @@
+def mayor_movilidad(transitos):
+    # Conteo de patentes #
+    conteo = {} # Diccionario para realizar conteo
+    for patente, portico in transitos: # Recorrido de la estructura
+        if patente not in conteo: # Si la patente no existe 
+            conteo[patente] = 0 # Creamos el contador
+        conteo[patente] += 1 # Sumamos uno cada vez que encontremos la patente
+    # Lista temporal para ordenar segun el conteo #
+    lista = [] # Estructura: [(conteo_1, patente_1), (conteo_2, patente_2), ...]
+    for patente in conteo: # Recorremos el diccionario
+        tupla = (conteo[patente], patente) # Dejamos el conteo primero para ordenar con sort
+        lista.append(tupla) # Agregamos la tupla
+    lista.sort() # Ordenamos ascendentemente
+    lista.reverse() # Ordenamos descendentemente
+    # Lista de salida segun el formato solicitado #
+    salida = []
+    for conteo, patente in lista[:3]: # Recorremos solo los primeros 3 que necesitamos
+        salida.append((patente, conteo)) # Volvemos al orden solicitado
+    return salida
+
+
+def reportar(transitos):
+    reporte = {} # Diccionario a retornar
+    for patente, portico in transitos: # Recorrido de la estructura
+        if portico not in reporte: # Si no existe el portico en el diccionario
+            reporte[portico] = [] # Se crea una lista vacia
+        if patente not in reporte[portico]: # Si la patente no existe en la lista
+            reporte[portico].append(patente) # Se agrega
+        reporte[portico].sort() # Ordenamos la lista de patentes en orden alfabetico
+    return reporte # Retornar diccionario
+
+# Estructura de prueba #
+transitos = [
+    ('BBJJ77', 2), 
+    ('CCHH19', 3), 
+    ('AAFF37', 3), 
+    ('BBJJ77', 1), 
+    ('AAFF37', 1), 
+    ('DDKK33', 3), 
+    ('AAFF37', 1), 
+    ('AAFF37', 2)
+]
+
+# Pruebas #
+print(mayor_movilidad(transitos))
+print(reportar(transitos))
