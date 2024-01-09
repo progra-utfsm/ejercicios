d340cbdd46403cd87bf92966966c00e449d9b792
Daniel San Martin <dsanmartinreyes@gmail.com>
2024-01-08 21:39:34 -0300

    Deployed 633c3ce with MkDocs version: 1.5.3

--- python/diccionarios/conductores.py
+++ python/diccionarios/conductores.py
@@ -1,15 +1,12 @@
-conductores = {
-    'azambrano': ('Andres Zambrano', 5.6, ('Hyundai', 'Elantra')),
-    'jojeda': ('Juan Ojeda', 1.1, ('Hyundai', 'Accent')),
-    # ...
-}
-
-
 def agrega_conductor(conductores, nuevo_conductor):
-    username, nombre, puntaje, (marca, modelo) = nuevo_conductor
+    username = nuevo_conductor[0]
+    nombre = nuevo_conductor[1]
+    puntaje = nuevo_conductor[2]
+    marca = nuevo_conductor[3][0]
+    modelo = nuevo_conductor[3][1]
     if username in conductores:
         return False
-    conductores[username] = (nombre, puntaje, (marca, modelo))
+    conductores[username] = [nombre, puntaje, [marca, modelo]]
     return True
 
 
@@ -23,10 +20,24 @@ def elimina_conductor(conductores, username):
 def ranking(conductores):
     r = []
     for c in conductores:
-        r.append((conductores[c][1], conductores[c][0]))
+        r.append([conductores[c][1], conductores[c][0]])
     r.sort()
     r.reverse()
     final = []
-    for puntaje, nombre in r:
+    for datos in r:
+        puntaje = datos[0]
+        nombre = datos[1]
         final.append((nombre, puntaje))
     return final
+
+
+conductores = {
+    'azambrano': ['Andres Zambrano', 5.6, ['Hyundai', 'Elantra']],
+    'jojeda': ['Juan Ojeda', 1.1, ['Hyundai', 'Accent']],
+    # ...
+}
+
+print(agrega_conductor(conductores,['fsoto', 'Fabiola Soto', 4.4, ['Peugeot', '308']]))
+print(elimina_conductor(conductores, 'jojeda'))
+print(elimina_conductor(conductores, 'pmunoz'))
+print(ranking(conductores))
--- python/diccionarios/viajes.py
+++ python/diccionarios/viajes.py
@@ -1,12 +1,3 @@
-viajes = {
-  # identificador_viaje: (origen, destino, minutos)
-  1323: ('santiago', 'lampa', 34),
-  7643: ('santiago', 'vitacura', 27),
-  9832: ('las condes', 'lampa', 45),
-  2221: ('renca', 'las condes', 38),
-  #...
-}
-
 def cuenta_minutos(viajes):
     c = {}
     for v in viajes:
@@ -15,5 +6,16 @@ def cuenta_minutos(viajes):
         c[viajes[v][1]] += viajes[v][2]
     final = []
     for i in c:
-        final.append((i, c[i]))
+        final.append([i, c[i]])
     return final
+
+viajes = {
+  # identificador_viaje: [origen, destino, minutos]
+  1323: ['santiago', 'lampa', 34],
+  7643: ['santiago', 'vitacura', 27],
+  9832: ['las condes', 'lampa', 45],
+  2221: ['renca', 'las condes', 38],
+  #...
+}
+
+print(cuenta_minutos(viajes))
