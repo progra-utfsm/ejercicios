68cfd1e25c04877a661bca69b524513b6423bd7a
Daniel San Martin <dsanmartinreyes@gmail.com>
2024-03-25 10:03:42 -0300

    Deployed a0d1b66 with MkDocs version: 1.5.3

--- python/condicionales/tiro.py
+++ python/condicionales/tiro.py
@@ -3,13 +3,13 @@ d = int(input("Ingrese la distancia: "))
 x = int(input("Ingrese  x: "))
 y = int(input("Ingrese  y: "))
 # Comprobar el cuadrante en el que se encuentra el punto
-if x > -d and x < d and y > -d and y < d:
+if abs(x) <= d and abs(y) <= d: # if x >= -d and x <= d and y >= -d and y= < d:
     print("Área 1")
-elif x > -2 * d and x < 2 * d and y > -2 * d and y < 2 * d:
+elif abs(x) <= 2 * d and abs(y) <= 2 * d: # elif x >= -2 * d and x <= 2 * d and y >= -2 * d and y <= 2 * d:
     print("Área 2")
-elif x > -3 * d and x < 3 * d and y > -3 * d and y < 3 * d:
+elif abs(x) <= 3 * d and abs(y) <= 3 * d: # elif x >= -3 * d and x <= 3 * d and y >= -3 * d and y <= 3 * d:
     print("Área 3")
-elif x > -4 * d and x < 4 * d and y > -4 * d and y < 4 * d:
+elif abs(x) <= 4 * d and abs(y) <= 4 * d: # elif x >= -4 * d and x <= 4 * d and y >= -4 * d and y <= 4 * d:
     print("Área 4")
 else:
     print("Fuera del área")
